e43079d563616601689d33c926c4c91567e2cf72
poligonforumowy <44902088+poligonforumowy@users.noreply.github.com>
2018-11-09 16:39:42 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 var test = 'test';
+alert(test);
 console.warn('test',test);
-javascript:console.warn('test',test);
 javascript:(function(){console.warn('test',test)})();
+javascript:console.warn('test',test);
